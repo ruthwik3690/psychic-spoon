96b1e28b2d7b912926802a76aeca1a11048599e9
Kaushik ramabhotla <44550184+kaushikramabhotla@users.noreply.github.com>
2020-10-02 12:29:00 +0530

    Update process.py

--- process.py
+++ process.py
@@ -45,3 +45,5 @@ from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train[:, 3:] = sc.fit_transform(X_train[:, 3:])
 X_test[:, 3:] = sc.transform(X_test[:, 3:])
+
+## Mere angane mein tumhara kya kaam hai??
